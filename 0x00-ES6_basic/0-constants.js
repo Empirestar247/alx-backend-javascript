35fb4d22b26c5f96a71ea31e066b9f363f3943c6
Empirestar247 <ejimofor81@mail.com>
2024-03-04 22:04:12 +0000

    ES6_basic

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,4 +1,15 @@
-export function myExportedFunction(parameter) {
-  // Function logic here
-  console.log(`Exported function called with parameter: ${parameter}`);
+export function taskFirst() {
+  const task = 'I prefer const when I can.';
+  return task;
+}
+
+export function getLast() {
+  return ' is okay';
+}
+
+export function taskNext() {
+  let combination = 'But sometimes let';
+  combination += getLast();
+
+  return combination;
 }
