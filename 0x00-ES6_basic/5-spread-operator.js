aaad7fb6b812cc208d974e34fa1e706f57c7c599
Empirestar247 <ejimofor81@mail.com>
2024-03-04 21:57:28 +0000

    ES6_basic

--- 0x00-ES6_basic/5-spread-operator.js
+++ 0x00-ES6_basic/5-spread-operator.js
@@ -1,5 +1,3 @@
-function concatenateArraysAndString(arr1, arr2, str) {
-  const result = [...arr1, ...arr2, ...str];
-  return result;
-}
+export default function concatArrays(array1, array2, string) {
+  return [...array1, ...array2, ...string];
 }
